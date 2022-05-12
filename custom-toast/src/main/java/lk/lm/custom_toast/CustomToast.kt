f5507720da9a2b37adb23bf227e159da91e10a5d
Lahiru <lahiru.muthumal@spemai.com>
2022-05-12 21:10:08 +0530

    initial 1

--- custom-toast/src/main/java/lk/lm/custom_toast/CustomToast.kt
+++ custom-toast/src/main/java/lk/lm/custom_toast/CustomToast.kt
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
 
-object CustomToast {
-    @SuppressLint("ShowToast")
-    fun print(context: Context, str:String){
-        Toast.makeText(context,str,Toast.LENGTH_LONG).show()
+class CustomToast {
+    companion object {
+        @SuppressLint("ShowToast")
+        fun print(context: Context, str: String) {
+            Toast.makeText(context, str, Toast.LENGTH_LONG).show()
+        }
     }
 }
